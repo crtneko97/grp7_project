d008d16bfe28dcec92b8675f68a2542e6ee42fa7
Mickemannen86 <112933571+Mickemannen86@users.noreply.github.com>
2023-03-29 15:39:00 +0200

    MickeSaysHallo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import { NextPage } from 'next'
+import { NextPage } from "next"
 
 interface Props {}
 
 const index: NextPage<Props> = ({}) => {
-  return <div>hello</div>
+  return <div>hello Simon from Mickenator</div>
 }
 
 export default index
