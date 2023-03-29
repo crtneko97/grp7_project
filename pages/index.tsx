12460af672ceb3e99fec39cd034d944031556675
crtneko97 <simon_kern@hotmail.com>
2023-03-29 18:00:02 +0200

    hälsning till micke

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,12 @@ import { NextPage } from "next"
 interface Props {}
 
 const index: NextPage<Props> = ({}) => {
-  return <div>hello Simon from Mickenator</div>
+  return (
+    <div>
+      hello Simon from Mickenator
+    <h1> Micke är fan bäst </h1>
+    </div>
+  )
 }
 
 export default index
