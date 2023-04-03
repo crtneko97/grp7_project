1de3b27bd7ee8b7a4c8ee6735250b732d343f02d
crtneko97 <simon_kern@hotmail.com>
2023-04-03 17:35:52 +0200

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import { NextPage } from "next"
 import BottomNavbar from "@/components/BottomNavbar"
-import PeppiStart from "@/components/PeppiStart"
 interface Props {}
 
 
 const index: NextPage<Props> = ({}) => {
   return (
   <div>
-    <div>
-        <PeppiStart />
-    </div>
     <div>
       <BottomNavbar />
     </div>
