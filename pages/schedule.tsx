550c74f98895cb78036053e3bcffb028671feca0
crtneko97 <simon_kern@hotmail.com>
2023-04-03 20:06:10 +0200

    micke & simon calendar/schedule push par programmering

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -1,9 +1,42 @@
-import { NextPage } from "next"
+import { useState } from "react";
+import moment from "moment";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { Event } from "../types/Event";
 
-interface Props {}
+const localizer = momentLocalizer(moment);
 
-const Schedule: NextPage<Props> = ({}) => {
-  return <div></div>
-}
+const CalendarPage = () => {
+  const [events, setEvents] = useState<Event[]>([]);
 
-export default Schedule
+  const handleSelect = ({ start, end }: any) => {
+    const title = window.prompt("Enter event title:");
+    if (title) {
+      const newEvent: Event = {
+        id: String(events.length + 1),
+        title,
+        start,
+        end,
+      };
+      setEvents([...events, newEvent]);
+    }
+  };
+
+  return (
+    <div>
+      <h1>My Calendar</h1>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        selectable
+        onSelectSlot={handleSelect}
+        style={{ height: "70vh" }}
+      />
+    </div>
+  );
+};
+
+export default CalendarPage;
+
